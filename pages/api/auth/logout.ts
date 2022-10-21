d1b5faf92b029f70e37f26b86d1afa3ef73af075
MARCOS JAMPIETRI <marcosjampietri@MARCOSs-MacBook-Pro.local>
2022-10-21 21:05:41 +0100

    removed logout api condition

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -3,29 +3,16 @@ import jwt, { Secret } from "jsonwebtoken";
 import { serialize } from "cookie";
 
 export default function logout(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { myTokenName } = req.cookies;
-    if (!myTokenName) {
-      console.log("no Token");
-      return res
-        .status(401)
-        .json({ status: "error", message: "not logged in" });
-    }
+  const serialized = serialize("myTokenName", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 0,
+    path: "/",
+  });
 
-    const serialized = serialize("myTokenName", "", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 0,
-      path: "/",
-    });
-
-    res.setHeader("Set-Cookie", serialized);
-    return res.status(200).json({
-      message: "Logout successful",
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("error");
-  }
+  res.setHeader("Set-Cookie", serialized);
+  return res.status(200).json({
+    message: "Logout successful",
+  });
 }
